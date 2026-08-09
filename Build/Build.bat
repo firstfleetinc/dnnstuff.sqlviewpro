c7784f11edbb8459e2a2ff32c7be8ee652cd5b31
Andrew Leverette <andrewleverette@gmail.com>
2026-08-09 08:50:32 -0400

    chore: fix build script to dynamically select tooling

--- Build/Build.bat
+++ Build/Build.bat
@@ -1,7 +1,6 @@
 @echo off
 echo.
 set version=%1
-set path=C:\Program Files (x86)\MSBuild\14.0\Bin\;%path%
 
 mkdir logs
 
@@ -11,7 +10,7 @@ REM restore packages
 nuget.exe restore ..\
 
 REM build install zip file
-Msbuild.exe ModuleSpecific.targets /p:VisualStudioVersion=14.0;Version=%version%;Configuration=%buildconfig%;TargetFrameworkVersion=v4.6 /t:Install /l:FileLogger,Microsoft.Build.Engine;logfile=logs\Build_%buildconfig%.log;verbosity=diagnostic
+Msbuild.exe ModuleSpecific.targets /p:VisualStudioVersion=%VisualStudioVersion%;Version=%version%;Configuration=%buildconfig%;TargetFrameworkVersion=v4.6.1 /t:Install /l:FileLogger,Microsoft.Build.Engine;logfile=logs\Build_%buildconfig%.log;verbosity=diagnostic
 if ERRORLEVEL 1 goto end
 
 :end
